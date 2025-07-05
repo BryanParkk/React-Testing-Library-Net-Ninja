149c18d22e439e2b42a41f39a09b90409caf79f9
Byeongil Park <bryan1010110@gmail.com>
2025-07-04 22:59:59 -0400

    update

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,41 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import Header from '../Header';
+import { render, screen } from "@testing-library/react";
+import Header from "../Header";
 
 describe("Header", () => {
-    it('should render same text passed into title prop', () => {
-        render(
-            <Header 
-              title="todo"
-            />
-        );
-        const h1Element = screen.getByText(/todo/i);
-        expect(h1Element).toBeInTheDocument();
-    });
-})
+  it("should render same text passed into title prop", () => {
+    render(<Header title="todo" />);
+    const headingElement = screen.getByText(/todo/i);
+    expect(headingElement).toBeInTheDocument();
+  });
+});
 
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Element = screen.getByRole("heading");
-//     expect(h1Element).toBeInTheDocument();
+it("should render same text passed into title prop", () => {
+  render(<Header title="todo" />);
+  const h1Element = screen.getByRole("heading");
+  expect(h1Element).toBeInTheDocument();
+});
+
+// it("should render same text passed into title prop", () => {
+//   render(<Header title="todo" />);
+//   const h1Element = screen.getByRole("heading", { name: /todo/i });
+//   expect(h1Element).toBeInTheDocument();
 // });
 
 // it('should render same text passed into title prop', () => {
 //     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Element = screen.getByRole("heading", { name: /todo/i });
-//     expect(h1Element).toBeInTheDocument();
-// });
-
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
+//         <Header
 //           title="todo"
 //         />
 //     );
@@ -45,7 +33,7 @@ describe("Header", () => {
 
 // it('should render same text passed into title prop', () => {
 //     render(
-//         <Header 
+//         <Header
 //           title="todo"
 //         />
 //     );
@@ -57,7 +45,7 @@ describe("Header", () => {
 
 // it('should render same text passed into title prop', async () => {
 //     render(
-//         <Header 
+//         <Header
 //           title="todo"
 //         />
 //     );
@@ -69,7 +57,7 @@ describe("Header", () => {
 
 // it('should render same text passed into title prop', () => {
 //     render(
-//         <Header 
+//         <Header
 //           title="todo"
 //         />
 //     );
@@ -81,7 +69,7 @@ describe("Header", () => {
 
 // it('should render same text passed into title prop', () => {
 //     render(
-//         <Header 
+//         <Header
 //           title="todo"
 //         />
 //     );
